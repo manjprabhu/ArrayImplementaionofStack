17f3a2f96f890e09e39a9377f151f42c44de88df
MPB <mp4learning@gmail.com>
2021-10-23 17:21:33 +0530

    Implement stack using Linkedlist

---
+++ app/src/main/java/com/example/arrayimplementaionofstack/StackUsingLinkedList.java
@@ -0,0 +1,37 @@
+package com.example.arrayimplementaionofstack;
+
+import android.util.Log;
+
+public class StackUsingLinkedList {
+
+    //Global reference to top
+    private Node top;
+
+    public void push(int element) {
+
+        Node node = new Node();
+
+        node.data = element;
+        node.link = top;
+
+        node = top;
+    }
+
+    public void pop() {
+        if (top == null) {
+            Log.v("Stack", " Stack is empty");
+        }
+
+        top = top.link;
+    }
+
+    public void peek() {
+
+    }
+
+    private class Node {
+        int data;
+        Node link;
+    }
+}
+
